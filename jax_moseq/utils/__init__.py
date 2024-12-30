4504b167d19ca74944835c9429e34497bff8499a
athp18 <129908089+athp18@users.noreply.github.com>
2024-12-29 17:47:54 -0800

    Update __init__.py

--- jax_moseq/utils/__init__.py
+++ jax_moseq/utils/__init__.py
@@ -1,2 +1,14 @@
+import os
+import joblib
+
 from .utils import *
 from .debugging import *
+
+def save_model(model, path):
+  assert os.path.exists(path)
+  if path.endswith('.p') or path.endswith('.pkl'):
+    joblib.dump(model, path)
+
+def load_model(filename):
+  if filename.endswith('.p') or filename.endswith('.pkl'):
+    return joblib.load(filename)
